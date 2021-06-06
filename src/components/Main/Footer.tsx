94be19eced84a5e6fb0773ecd6643a36c4a13705
Shubham Kumar <kumar1234shubham@gmail.com>
2021-06-06 18:19:48 +0530

    footer text

--- src/components/Main/Footer.tsx
+++ src/components/Main/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <div style={{ height:60, background: "#f2f2f2", position:"absolute", bottom:0, left: 0, right: 0}}>
-
+        <div style={{ height:60, background: "#f2f2f2", position:"absolute", bottom:0, left: 0, right: 0, display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <p>@copyright: Chip Casino</p>
         </div>
     )
 }
